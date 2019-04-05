1556772be579908f6766bf9bf8a5128f0d0549f2
Divya <alaganduladivyamadhuri@gmail.com>
2019-04-05 14:34:29 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -7,6 +7,14 @@ function forLoop(array){
       array.push(`I am ${i} strange loops.`)
     }
   }
-  
+
   return array
+}
+
+function whileLoop(n){
+  while (n>0)
+  {
+    console.log(--n);
+  }
+  return 'done'
 }
