33f2f31567097cbe0d5e6e928ee31b98eb91be6d
Divya <alaganduladivyamadhuri@gmail.com>
2019-04-05 14:26:36 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -3,6 +3,8 @@ function forLoop(array){
   for (let i=0;i<25;i++){
     if(i=1){
       return array + "I am" + ${i} + "strange loop"
+    } else {
+      return array + "I am" + ${i} + "strange loops"
     }
   }
 }
